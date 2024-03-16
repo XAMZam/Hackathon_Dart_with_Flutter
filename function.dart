d8384ca9ba9486e0c96ace11e9fe5236797122f7
ZAMZAM ALI HEDAR <133240996+XAMZam@users.noreply.github.com>
2024-03-16 04:48:26 -0700

    dart with flutter

--- function.dart
+++ function.dart
@@ -1,23 +1,36 @@
 void main() {
   // Define the student's marks
-  double marks = 88;
+  double marks = 55; // Assign a value to marks
+
+  // Print out the value of marks
+  print("Marks: $marks");
 
   // Determine the grade based on the marks
   String grade = calculateGrade(marks);
 
-  // Print out the grade
+  // Print out the determined grade
   print("Grade: $grade");
 }
 
 // Function to calculate the grade based on marks
 String calculateGrade(double marks) {
-  if (marks > 85) {
-    return "Excellent";
-  } else if (marks >= 75 && marks <= 85) {
-    return "Very Good";
-  } else if (marks >= 65 && marks < 75) {
-    return "Good";
-  } else {
-    return "Average";
+  String grade;
+
+  switch (marks ~/ 10) {
+    case 10:
+    case 9:
+    case 8:
+      grade = "Excellent";
+      break;
+    case 7:
+      grade = "Very Good";
+      break;
+    case 6:
+      grade = "Good";
+      break;
+    default:
+      grade = "Average";
   }
+
+  return grade;
 }
